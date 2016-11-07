58f51926630d5e2945b13737ead30c75ddce0da4
iamquang95 <iamquang95@gmail.com>
2016-11-08 01:41:36 +0700

    Refine extracion

--- ExtractData.py
+++ ExtractData.py
@@ -32,19 +32,36 @@ def devideIgnoreZero(arg1, arg2):
 def getLayerAsFloatArray(filename, layerStr):
     return np.array(getArrayLayer(filename, layerStr)).astype(float)
 
+# This function can only extract layer from 500m MODIS 8 dayus
+def getAllLayers(filename):
+    redLayerStr = "MOD_Grid_500m_Surface_Reflectance:sur_refl_b01"
+    nirLayerStr = "MOD_Grid_500m_Surface_Reflectance:sur_refl_b02"
+    blueLayerStr = "MOD_Grid_500m_Surface_Reflectance:sur_refl_b03"
+    swirLayerStr = "MOD_Grid_500m_Surface_Reflectance:sur_refl_b06"
+
+    redLayer = getLayerAsFloatArray(filename, redLayerStr)
+    nirLayer = getLayerAsFloatArray(filename, nirLayerStr)
+    blueLayer = getLayerAsFloatArray(filename, blueLayerStr)
+    swirLayer = getLayerAsFloatArray(filename, swirLayerStr)
+
+    ndviLayer = devideIgnoreZero((nirLayer - redLayer), (nirLayer + redLayer))
+    eviLayer = devideIgnoreZero((2.5*(nirLayer-redLayer)), (nirLayer + 6*redLayer - 7.5*blueLayer + 1))
+    lswiLayer = devideIgnoreZero((nirLayer - swirLayer), (nirLayer + swirLayer))
+
+    return {
+        "redLayer": redLayer,
+        "nirlayer": nirLayer,
+        "blueLayer": blueLayer,
+        "swirLayer": swirLayer,
+        "ndviLayer": ndviLayer,
+        "eviLayer": eviLayer,
+        "lswiLayer": lswiLayer
+    }
+
+# Test with one image
+
 filename = "MOD09A1.A2015017.h27v06.005.2015028030040.hdf"
-redLayerStr = "MOD_Grid_500m_Surface_Reflectance:sur_refl_b01"
-nirLayerStr = "MOD_Grid_500m_Surface_Reflectance:sur_refl_b02"
-blueLayerStr = "MOD_Grid_500m_Surface_Reflectance:sur_refl_b03"
-swirLayerStr = "MOD_Grid_500m_Surface_Reflectance:sur_refl_b06"
-
-# printAllSubData("MOD09A1.A2015017.h27v06.005.2015028030040.hdf")
-redLayer = getLayerAsFloatArray(filename, redLayerStr)
-nirLayer = getLayerAsFloatArray(filename, nirLayerStr)
-blueLayer = getLayerAsFloatArray(filename, blueLayerStr)
-swirLayer = getLayerAsFloatArray(filename, swirLayerStr)
-
-ndviLayer = devideIgnoreZero((nirLayer - redLayer), (nirLayer + redLayer))
-eviLayer = 2.5*devideIgnoreZero((nirLayer-redLayer), (nirLayer + 6*redLayer - 7.5*blueLayer + 1))
-lswiLayer = devideIgnoreZero((nirLayer - swirLayer), (nirLayer + swirLayer))
+x = getAllLayers(filename)
+print(x["eviLayer"])
+showImage(x["ndviLayer"])
 
